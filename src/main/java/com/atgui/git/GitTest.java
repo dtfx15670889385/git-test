376bc71baa5bdb49291cc9bbae4e6c9ad9185213
HH <354873316@qq.com>
2022-07-24 09:33:09 +0800

    second commit

--- src/main/java/com/atgui/git/GitTest.java
+++ src/main/java/com/atgui/git/GitTest.java
@@ -5,4 +5,8 @@ package com.atgui.git;
  * @create 2022-07-24 9:14
  */
 public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello world!");
+    }
 }
